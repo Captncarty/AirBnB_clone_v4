0ddbcad6274434581ab808a74f84c62e9d1cd424
kelvin <edithmukoro3@gmail.com>
2023-05-06 00:47:39 +0000

    all done

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,16 +8,15 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
+app = Flask(__name__)
 
-app = Flask(__name
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/101-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -32,11 +32,10 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('101-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places,
-			   cache_id=uuid.uuid4())
+                           places=places, cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -8,15 +8,15 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+app = Flask(__name__)
 
-app = Flask(__name
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/2-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -32,7 +32,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
